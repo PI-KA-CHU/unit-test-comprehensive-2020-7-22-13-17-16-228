cb685ebadd586397b8907d2d17c0d22f475646f6
benjamin <529709737@qq.com>
2020-07-23 19:10:41 +0800

    [guess number][Benjaimn] feature: pass should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234

--- src/main/java/com/oocl/unitTest/GuessNumber.java
+++ src/main/java/com/oocl/unitTest/GuessNumber.java
@@ -2,6 +2,12 @@ package com.oocl.unitTest;
 
 public class GuessNumber {
     public String guess(int[] inputGuess, int[] answer) {
-        return null;
+
+        for (int i = 0; i < answer.length; i++) {
+            if (inputGuess[i] != answer[i]) {
+                return null;
+            }
+        }
+        return "4A0B";
     }
 }
