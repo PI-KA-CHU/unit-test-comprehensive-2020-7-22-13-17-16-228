a5af4db96433280531fe0957401683346973c8c9
benjamin <529709737@qq.com>
2020-07-23 19:39:33 +0800

    [guess number][Benjaimn] refactor: class GuessNumber's guess method.

--- src/main/java/com/oocl/unitTest/GuessNumber.java
+++ src/main/java/com/oocl/unitTest/GuessNumber.java
@@ -1,22 +1,27 @@
 package com.oocl.unitTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GuessNumber {
     public String guess(int[] inputGuess, int[] answer) {
 
         int countAllCurrect = 0;
-        int countNumberEqualAndIndexNot = 0;
+        int countNumberEqual = 0;
+        Map<Integer, Integer> numberIndexMap = new HashMap<>();
 
         for (int i = 0; i < answer.length; i++) {
-            for (int j = 0; j < inputGuess.length; j++) {
-                if (answer[i] == inputGuess[j]) {
-                    if (i == j) {
-                        countAllCurrect ++;
-                    }else {
-                        countNumberEqualAndIndexNot ++;
-                    }
-                }
+            if (answer[i] == inputGuess[i]) {
+                countAllCurrect ++;
+            }
+            numberIndexMap.put(answer[i], i);
+        }
+
+        for (int i = 0; i < inputGuess.length; i++) {
+            if (numberIndexMap.get(inputGuess[i]) != null) {
+                countNumberEqual ++;
             }
         }
-        return String.format("%sA%sB",countAllCurrect,countNumberEqualAndIndexNot);
+        return String.format("%sA%sB",countAllCurrect,countNumberEqual - countAllCurrect);
     }
 }
