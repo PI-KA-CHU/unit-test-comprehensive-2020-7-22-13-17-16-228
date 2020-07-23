0f6ee4be47d5f823a3f5bae94a223c5f1c1ffb7a
benjamin <529709737@qq.com>
2020-07-23 21:02:44 +0800

    [generate answer][Benjaimn] feature: realize RealAnswerGenerator's genetare methond

--- src/main/java/com/oocl/unitTest/RealAnswerGenerator.java
+++ src/main/java/com/oocl/unitTest/RealAnswerGenerator.java
@@ -3,6 +3,26 @@ package com.oocl.unitTest;
 public class RealAnswerGenerator implements AnswerGenerator {
     @Override
     public int[] generate() {
-        return null;
+        int length = 4;
+        int maxNum = 9;
+        int minNum = 0;
+        int[] result = new int[length];
+
+        int count = 0;
+        while(count < length) {
+            int num = (int) (Math.random() * (maxNum - minNum)) + minNum;
+            boolean flag = true;
+            for (int j = 0; j < length; j++) {
+                if(num == result[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
     }
 }
