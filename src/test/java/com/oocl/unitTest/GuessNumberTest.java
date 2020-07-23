3d8c726512005b5251e8a4d4885e5ec9895caedb
benjamin <529709737@qq.com>
2020-07-23 21:21:52 +0800

    [guess number][Benjaimn] test: add the mock to generate answer

--- src/test/java/com/oocl/unitTest/GuessNumberTest.java
+++ src/test/java/com/oocl/unitTest/GuessNumberTest.java
@@ -2,16 +2,21 @@ package com.oocl.unitTest;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 public class GuessNumberTest {
 
     @Test
     void should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234(){
         GuessNumber guessNumber = new GuessNumber();
-        int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {1, 2, 3, 4};
+        int[] answer = {1, 2, 3, 4};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("4A0B", result);
     }
@@ -21,8 +26,10 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {1, 2, 4, 3};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("2A2B", result);
     }
@@ -32,8 +39,10 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {4, 3, 2, 1};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("0A4B", result);
     }
@@ -43,8 +52,10 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {1, 5, 6, 2};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("1A1B", result);
     }
@@ -54,8 +65,10 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {2, 1, 5, 6};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("0A2B", result);
     }
@@ -65,8 +78,10 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
         int[] inputGuess = {5, 6, 7, 8};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
 
-        String result = guessNumber.guess(inputGuess, answer);
+        String result = guessNumber.guess(inputGuess, answerGenerator.generate());
 
         Assertions.assertEquals("0A0B", result);
     }
