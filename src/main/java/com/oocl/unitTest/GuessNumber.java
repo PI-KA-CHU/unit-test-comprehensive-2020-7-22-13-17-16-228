2e125f4320165ced90c9d22897fd5a0074e35797
benjamin <529709737@qq.com>
2020-07-23 19:25:02 +0800

    [guess number][Benjaimn] feature: pass should_return_2A2B_when_guess_given_answer_1234_and_input_guess_1243

--- src/main/java/com/oocl/unitTest/GuessNumber.java
+++ src/main/java/com/oocl/unitTest/GuessNumber.java
@@ -3,11 +3,20 @@ package com.oocl.unitTest;
 public class GuessNumber {
     public String guess(int[] inputGuess, int[] answer) {
 
+        int countAllCurrect = 0;
+        int countNumberEqualAndIndexNot = 0;
+
         for (int i = 0; i < answer.length; i++) {
-            if (inputGuess[i] != answer[i]) {
-                return null;
+            for (int j = 0; j < inputGuess.length; j++) {
+                if (answer[i] == inputGuess[j]) {
+                    if (i == j) {
+                        countAllCurrect ++;
+                    }else {
+                        countNumberEqualAndIndexNot ++;
+                    }
+                }
             }
         }
-        return "4A0B";
+        return String.format("%sA%sB",countAllCurrect,countNumberEqualAndIndexNot);
     }
 }
