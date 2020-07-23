c0260243ed9da7d01d543c26f43a8c20cac833e2
benjamin <529709737@qq.com>
2020-07-23 20:04:01 +0800

    [legal check][Benjaimn] feature: pass should_return_true_when_check_given_input_guess_1234

--- src/main/java/com/oocl/unitTest/LegalChecker.java
+++ src/main/java/com/oocl/unitTest/LegalChecker.java
@@ -1,8 +1,25 @@
 package com.oocl.unitTest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LegalChecker {
 
     public boolean checkIsLegal(int[] inputNumber) {
-        return false;
+        if (inputNumber == null || inputNumber.length != 4) {
+            return false;
+        }
+
+        Set<Integer> inputNumberSet = new HashSet<>();
+        for (int i = 0; i < inputNumber.length; i++) {
+            if (inputNumber[i] < 0 || inputNumber[i] > 9) {
+                return false;
+            }
+            if (inputNumberSet.contains(inputNumber[i])) {
+                return false;
+            }
+            inputNumberSet.add(inputNumber[i]);
+        }
+        return true;
     }
 }
