c0e55ae02588b3be084c9eb1e3b4eb94eacb835d
benjamin <529709737@qq.com>
2020-07-23 19:17:28 +0800

    [guess number][Benjaimn] test: should_return_2A2B_when_guess_given_answer_1234_and_input_guess_1256

--- src/test/java/com/oocl/unitTest/GuessNumberTest.java
+++ src/test/java/com/oocl/unitTest/GuessNumberTest.java
@@ -15,4 +15,15 @@ public class GuessNumberTest {
 
         Assertions.assertEquals("4A0B", result);
     }
+
+    @Test
+    void should_return_2A2B_when_guess_given_answer_1234_and_input_guess_1256(){
+        GuessNumber guessNumber = new GuessNumber();
+        int[] answer = {1, 2, 3, 4};
+        int[] inputGuess = {1, 2, 5, 6};
+
+        String result = guessNumber.guess(inputGuess, answer);
+
+        Assertions.assertEquals("2A2B", result);
+    }
 }
