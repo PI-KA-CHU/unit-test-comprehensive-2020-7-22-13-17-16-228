2185255e075802106cd8e2cf3496f977b48b2e17
benjamin <529709737@qq.com>
2020-07-23 19:06:56 +0800

    [guess number][Benjaimn] test: should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234

---
+++ src/main/java/com/oocl/unitTest/GuessNumber.java
@@ -0,0 +1,7 @@
+package com.oocl.unitTest;
+
+public class GuessNumber {
+    public String guess(int[] inputGuess, int[] answer) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/unitTest/GuessNumberTest.java
@@ -0,0 +1,18 @@
+package com.oocl.unitTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GuessNumberTest {
+
+    @Test
+    void should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234(){
+        GuessNumber guessNumber = new GuessNumber();
+        int[] answer = {1, 2, 3, 4};
+        int[] inputGuess = {1, 2, 3, 4};
+
+        String result = guessNumber.guess(inputGuess, answer);
+
+        Assertions.assertEquals("4A0B", result);
+    }
+}
