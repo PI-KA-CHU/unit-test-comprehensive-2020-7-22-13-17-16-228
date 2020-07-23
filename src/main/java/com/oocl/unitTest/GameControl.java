f11168b4d684a2783435d0878b2f87b5a5ca6854
benjamin <529709737@qq.com>
2020-07-23 21:56:51 +0800

    [game control][Benjaimn] test: should_return_false_when_play_game_given_input_guess_1234_answer_1234_remainintTime_4

---
+++ src/main/java/com/oocl/unitTest/GameControl.java
@@ -0,0 +1,22 @@
+package com.oocl.unitTest;
+
+public class GameControl {
+
+    private int remainingTimes;
+
+    public GameControl(int remainingTimes) {
+        this.remainingTimes = remainingTimes;
+    }
+
+    public int getRemainingTimes() {
+        return remainingTimes;
+    }
+
+    public void setRemainingTimes(int remainingTimes) {
+        this.remainingTimes = remainingTimes;
+    }
+
+    public String playGame(int[] inputGuess, int[] generate) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/unitTest/GameControlTest.java
@@ -0,0 +1,23 @@
+package com.oocl.unitTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
+public class GameControlTest {
+
+    @Test
+    void should_return_false_when_play_game_given_input_guess_1234_answer_1234_remainintTime_4(){
+        int remainingTimes = 4;
+        GameControl gameControl = new GameControl(remainingTimes);
+        int[] inputGuess = {1, 2, 3, 4};
+        int[] answer = {1, 2, 3, 4};
+        AnswerGenerator answerGenerator = Mockito.mock(RealAnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+
+        String outputAfterPlayGame = gameControl.playGame(inputGuess,answerGenerator.generate());
+        Assertions.assertEquals("4A0B",outputAfterPlayGame);
+    }
+}
