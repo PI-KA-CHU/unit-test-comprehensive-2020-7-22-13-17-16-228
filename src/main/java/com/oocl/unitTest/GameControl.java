9ef4dad4bb71beba010bb8433dac70569f65bbf8
benjamin <529709737@qq.com>
2020-07-23 22:07:45 +0800

    [game control][Benjaimn] feature: finish GameControl' method playGame and pass should_return_4A0B_when_play_game_given_input_guess_1234_answer_1234_remainintTime_4

--- src/main/java/com/oocl/unitTest/GameControl.java
+++ src/main/java/com/oocl/unitTest/GameControl.java
@@ -3,9 +3,11 @@ package com.oocl.unitTest;
 public class GameControl {
 
     private int remainingTimes;
+    private GuessNumber guessNumber;
 
     public GameControl(int remainingTimes) {
         this.remainingTimes = remainingTimes;
+        this.guessNumber = new GuessNumber();
     }
 
     public int getRemainingTimes() {
@@ -16,7 +18,20 @@ public class GameControl {
         this.remainingTimes = remainingTimes;
     }
 
-    public String playGame(int[] inputGuess, int[] generate) {
-        return null;
+    public String playGame(int[] inputGuess, int[] answer) {
+        if (hasRemainingTimes()) {
+            setRemainingTimes(this.remainingTimes - 1);
+            return guessNumber.guess(inputGuess, answer) + "\n";
+        }
+
+        return "Wrong Input，Input again\n";
+    }
+
+    private boolean hasRemainingTimes(){
+        if (this.remainingTimes > 0) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/test/java/com/oocl/unitTest/GameControlTest.java
+++ src/test/java/com/oocl/unitTest/GameControlTest.java
@@ -9,7 +9,7 @@ import static org.mockito.Mockito.when;
 public class GameControlTest {
 
     @Test
-    void should_return_false_when_play_game_given_input_guess_1234_answer_1234_remainintTime_4(){
+    void should_return_4A0B_when_play_game_given_input_guess_1234_answer_1234_remainintTime_4(){
         int remainingTimes = 4;
         GameControl gameControl = new GameControl(remainingTimes);
         int[] inputGuess = {1, 2, 3, 4};
@@ -18,6 +18,6 @@ public class GameControlTest {
         when(answerGenerator.generate()).thenReturn(answer);
 
         String outputAfterPlayGame = gameControl.playGame(inputGuess,answerGenerator.generate());
-        Assertions.assertEquals("4A0B",outputAfterPlayGame);
+        Assertions.assertEquals("4A0B\n",outputAfterPlayGame);
     }
 }
