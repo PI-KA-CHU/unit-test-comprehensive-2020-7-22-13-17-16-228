194ce99de161701dd791331ab7e73aac8f007e96
benjamin <529709737@qq.com>
2020-07-27 22:04:29 +0800

    [legal checker][Benjamin] feature: Add some annotations to LegalChecker

--- src/test/java/com/oocl/unitTest/LegalCheckerTest.java
+++ src/test/java/com/oocl/unitTest/LegalCheckerTest.java
@@ -10,51 +10,66 @@ public class LegalCheckerTest {
 
     @Test
     void should_return_true_when_check_given_input_guess_1234(){
+        //given
         LegalChecker legalChecker = new LegalChecker();
         List<Integer> inputNumber = Arrays.asList(1, 2, 3, 4);
 
+        //when
         boolean isLegal = legalChecker.checkIsLegal(inputNumber);
 
+        //then
         Assertions.assertTrue(isLegal);
     }
 
     @Test
     void should_return_false_when_check_given_input_guess_12345(){
+        //given
         LegalChecker legalChecker = new LegalChecker();
         List<Integer> inputNumber = Arrays.asList(1, 2, 3, 4, 5);
 
+        //when
         boolean isLegal = legalChecker.checkIsLegal(inputNumber);
 
+        //then
         Assertions.assertFalse(isLegal);
     }
 
     @Test
     void should_return_false_when_check_given_input_guess_123(){
+        //given
         LegalChecker legalChecker = new LegalChecker();
         List<Integer> inputNumber = Arrays.asList(1, 2, 3);
 
+        //when
         boolean isLegal = legalChecker.checkIsLegal(inputNumber);
 
+        //then
         Assertions.assertFalse(isLegal);
     }
 
     @Test
     void should_return_false_when_check_given_input_guess_1123(){
+        //given
         LegalChecker legalChecker = new LegalChecker();
         List<Integer> inputNumber = Arrays.asList(1, 1, 2, 3);
 
+        //when
         boolean isLegal = legalChecker.checkIsLegal(inputNumber);
 
+        //then
         Assertions.assertFalse(isLegal);
     }
 
     @Test
     void should_return_false_when_check_given_input_guess_11023(){
+        //given
         LegalChecker legalChecker = new LegalChecker();
         List<Integer> inputNumber = Arrays.asList(1, 10, 2, 3);
 
+        //when
         boolean isLegal = legalChecker.checkIsLegal(inputNumber);
 
+        //then
         Assertions.assertFalse(isLegal);
     }
 }
